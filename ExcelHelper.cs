11fee8af4af024658a2e767fe2d86cdb52537b7d
EnricoAlborghetti <Enrico.alborghetti@gmail.com>
2026-04-16 16:52:44 +0200

    fix: update tab color assignment to support XSSFSheet color properties

--- ExcelHelper.cs
+++ ExcelHelper.cs
@@ -536,7 +536,14 @@ namespace ExcelPdf
 
             try
             {
-                newSheet.TabColorIndex = sourceSheet.TabColorIndex;
+                if (sourceSheet is XSSFSheet xssfSource && newSheet is XSSFSheet xssfDest)
+                {
+                    xssfDest.TabColor = xssfSource.TabColor;
+                }
+                else
+                {
+                    newSheet.TabColorIndex = sourceSheet.TabColorIndex;
+                }
             }
             catch { }
 
